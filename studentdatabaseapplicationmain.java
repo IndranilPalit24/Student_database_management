4dda933477f7fe60b40c091c8490f6bc7800603a
Indranil_Palit <73847100+IndranilPalit24@users.noreply.github.com>
2021-05-11 20:20:19 +0530

    Add files via upload

---
+++ studentdatabaseapplicationmain.java
@@ -0,0 +1,12 @@
+package Student_database_management_app;
+
+public class studentdatabaseapplicationmain {
+
+	public static void main(String[] args) {
+		//Ask how many users need to be added
+				Student_details stu1 = new Student_details();	
+				stu1.enroll();
+				//Create n number of students
+
+			}
+}
